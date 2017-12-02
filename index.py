713644df686b774ed207f76a7263a567628cc6ef
Markus Fix <lispmeister@gmail.com>
2017-12-02 19:02:34 +0400

    third commit

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
-        'timestamp1': datetime.datetime.utcnow().isoformat()
-        'timestamp2': datetime.datetime.utcnow().isoformat()
+        'timestamp1': datetime.datetime.utcnow().isoformat(),
+        'timestamp2': datetime.datetime.utcnow().isoformat(),
         'timestamp3': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
